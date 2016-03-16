527f859113ce4eeff5253937331c8a99a10ca449
renato <renato@servedo.in>
2016-03-16 09:23:42 -0300

    Atualizando projeto

--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
+$( document ).ready(function() {
+	/*$.ajax({
+	        url: "js/challenge.json",
+	        dataType: 'json',
+		success: function(dados) {
+//		console.log(dados.data.widget.size);
+			$('.qt-estoque').append('Atualmente nossa loja contem '+dados.data.widget.size+' produto(s) em estoque, aproveite nossos precos!');
+			$(dados.data.recommendation).each(function(i,f) {
+				$('.produtos .thumbnail img').attr('src', f.imageName);
+				//$('.produtos .titulo').append(f.name+"<br><br>");
+				//$('.produtos .codigo').append(f.businessId+"<br><br>");
+				//$('.produtos a').attr('href', f.detailUrl);
+				//$('.produtos a').attr('href', f.detailUrl);
+				/*
+				console.log(f.price);
+				console.log(f.oldPrice);
+				console.log(f.productInfo.paymentConditions);
+			})
+		}
+	});*/
+
+(function () {
+            console.log('Start Test');
+
+            var url = 'http://roberval.chaordicsystems.com/challenge/challenge.json?callback=X'
+
+                handleData = function (data) {
+		$('.qt-estoque').append('Atualmente nossa loja contem '+data.data.widget.size+' produto(s) em estoque, aproveite nossos precos!');
+                    console.log('Data: ', data);
+                },
+
+                X = handleData;
+
+            $.ajax({
+                url: url,
+                dataType: 'jsonp',
+            });
+        })();
 
 
-$(document).ready(function() {
 
-    var jqxhr = $.getJSON( "http://roberval.chaordicsystems.com/challenge/challenge.json?callback=X", function(data) {
-      console.log( "success", data );
-    })
-      .fail(function(textStatus, errorThrown) {
-        console.log("error " + textStatus, errorThrown);
-      })
 
 });
